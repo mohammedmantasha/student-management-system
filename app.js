1a89d977ca511431db9bf086238cfb92326d84e1
Mantasha <mohammed.mantasha24@spit.ac.in>
2026-02-18 15:34:52 +0530

    Changed role to type and messages

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 function login(user) {
-  if (user.role === "student") {
-    return "Welcome Student";
+  if (user.type === "student") {
+    return "Student Access";
   }
+<<<<<<< HEAD
   if (user.role === "admin") {
     return "Welcome Admin";
   }
   return "Access Denied";
+=======
+  if (user.type === "admin") {
+    return "Admin Access";
+  }
+  return "Unauthorized";
+>>>>>>> c2e2d97 (Changed role to type and messages)
 }
 module.exports = login;
